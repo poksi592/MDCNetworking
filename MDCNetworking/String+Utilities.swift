6031182e3cfcd36271792261c491e3f9fc3cee9e
Despotovic, Mladen <Mladen.Despotovic@tuitravel.com>
2016-12-20 11:51:59 +0000

    Added creating string URL Parameters

---
+++ MDCNetworking/String+Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  String+Utilities.swift
+//  MDCNetworking
+//
+//  Created by Despotovic, Mladen on 20/12/2016.
+//  Copyright © 2016 Despotovic, Mladen. All rights reserved.
+//
+
+import Foundation
---
+++ MDCNetworkingTests/DictionaryUtilitiesTests.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftUtilitiesTests.swift
+//  MDCNetworking
+//
+//  Created by Despotovic, Mladen on 20/12/2016.
+//  Copyright © 2016 Despotovic, Mladen. All rights reserved.
+//
+
+import XCTest
+
+class StringUtilitiesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
