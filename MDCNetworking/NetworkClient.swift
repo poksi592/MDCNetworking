cffc3c96a1a1a4a79b845d4e319ee6fb3db425c2
Bartomiej Nowak <bartlomiej.nowak@intive.com>
2018-02-22 10:07:17 +0100

    Added session method to NetworkClient

--- MDCNetworking/NetworkClient.swift
+++ MDCNetworking/NetworkClient.swift
@@ -10,9 +10,35 @@ import Foundation
 
 open class NetworkClient {
     
-    open private (set) var configuration: Configuration
+    open let configuration: Configuration
+    open let sessionProvider: URLSessionProvider?
     
-    public init(configuration: Configuration) {
+    public init(configuration: Configuration, sessionProvider: URLSessionProvider?) {
         self.configuration = configuration
+        self.sessionProvider = sessionProvider
+    }
+    
+    open func session(
+        urlPath: String,
+        method: HTTPMethod = .get,
+        parameters: [String: String]? = nil,
+        body: Data? = nil,
+        session: URLSession? = nil,
+        completion: @escaping ResponseCallback
+    ) -> HTTPSession {
+        
+        let session = HTTPSession(
+            urlPath: urlPath,
+            method: method,
+            parameters: parameters,
+            body: body,
+            configuration: configuration,
+            session: session,
+            completion: completion
+        )
+        
+        session.sessionProvider = sessionProvider
+        
+        return session
     }
 }
